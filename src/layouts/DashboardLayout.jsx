bb1a1bc0d01f74447a1819447b91519639d8161f
Mahir <mahir.daiyan9011@gmail.com>
2025-12-18 18:36:13 +0600

    created auth and dashboard layout

---
+++ src/layouts/DashboardLayout.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Navbar from "../components/Navbar";
+
+const DashboardLayout = ({ children }) => {
+  return (
+    <div className="min-h-screen bg-gradient-to-b from-gray-50 to-white dark:from-slate-900 dark:to-slate-800">
+      <Navbar />
+      {/* Main Content */}
+      <main className="pt-16">
+        <div className="p-6">{children}</div>
+      </main>
+    </div>
+  );
+};
+
+export default DashboardLayout;
